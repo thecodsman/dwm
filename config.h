4709dfcf815355cca399cf600781cd4fd5a0ede5
cody <cjonesfatloaf@gmail.com>
2024-05-27 01:28:27 -0400

    made the layout look a little nicer

--- config.h
+++ config.h
@@ -169,12 +169,12 @@ static const Rule rules[] = {
  */
 static const BarRule barrules[] = {
 	/* monitor   bar    alignment         widthfunc                 drawfunc                clickfunc                hoverfunc                name */
-	{ -1,        0,     BAR_ALIGN_LEFT,   width_tags,               draw_tags,              click_tags,              hover_tags,              "tags" },
+    {  0,        0,     BAR_ALIGN_LEFT,  width_systray,            draw_systray,           click_systray,           NULL,                    "systray" },
 	{ -1,        0,     BAR_ALIGN_LEFT,   width_ltsymbol,           draw_ltsymbol,          click_ltsymbol,          NULL,                    "layout" },
 	{ statusmon, 0,     BAR_ALIGN_RIGHT,  width_status2d,           draw_status2d,          click_statuscmd,         NULL,                    "status2d" },
 	{ -1,        0,     BAR_ALIGN_NONE,   width_wintitle,           draw_wintitle,          click_wintitle,          NULL,                    "wintitle" },
-	{ statusmon, 1,     BAR_ALIGN_RIGHT,  width_status2d_es,        draw_status2d_es,       click_statuscmd_es,      NULL,                    "status2d_es" },
-	{  0,        1,     BAR_ALIGN_LEFT,   width_systray,            draw_systray,           click_systray,           NULL,                    "systray" },
+	//{ statusmon, 1,     BAR_ALIGN_RIGHT,  width_status2d_es,        draw_status2d_es,       click_statuscmd_es,      NULL,                    "status2d_es" },
+    { -1,        1,     BAR_ALIGN_CENTER, width_tags,               draw_tags,              click_tags,              hover_tags,              "tags" },
 };
 
 /* layout(s) */
